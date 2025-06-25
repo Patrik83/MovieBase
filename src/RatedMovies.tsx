b82fd06259150d51085e2aedbcffeac1bd5b5355
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-25 14:57:56 +0200

    Created RatedMovies component which renders out the movies with a responsive layout

---
+++ src/RatedMovies.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query"
+import { getTopRated } from "./services/TMDB_API"
+
+const RatedMovies = () => {
+  const { data, error, isError, isLoading } = useQuery({
+    queryKey: (["movies"]),
+    queryFn: () => getTopRated(),
+  })
+
+  return (
+    <>
+      {isError && <p>{error.message}</p>}
+
+      {isLoading && <p>Movies are loading...</p>}
+
+      {data && (
+        <>
+          <title>Rated Movies</title>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 px-4 pt-3 min-w-[400px]">
+            {data.results.map(d => (
+              <div key={d.id}>
+                <img
+                  className="w-full"
+                  src={`https://image.tmdb.org/t/p/w500/${d.poster_path}`}
+                  alt=""
+                />
+                <div className="px-1 py-0">
+                  <h2 className="font-bold">{d.title}</h2>
+                  <div className="flex justify-between">
+                    <p className="text-gray-700 text-sm">{d.release_date}</p>
+                    <p className="text-gray-700 text-sm">Vote({d.vote_count})</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default RatedMovies;
