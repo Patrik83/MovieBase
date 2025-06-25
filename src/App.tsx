c6cb344d37d75814a957cdd45c3c5f40d654e7f4
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-25 15:01:04 +0200

    Added RatedMovies component as a route in App and wrapped a div with tailwind a container class around Routes, so that every Route will have the same layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import { Routes, Route } from "react-router";
+import RatedMovies from "./RatedMovies";
 
+function App() {
   return (
-    <>
-    </>
+    <div className="@container max-w-screen-xl mx-auto">
+      <Routes>
+        <Route path="/" element={<RatedMovies /> } />
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
