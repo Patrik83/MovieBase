820d1b4964e2b247c335a2660dca5d515d2d17be
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-25 20:14:08 +0200

    Added TrendingMoviesPage as a route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
-import RatedMovies from "./RatedMovies";
+import RatedMoviesPage from "./RatedMoviesPage";
+import TrendingMoviesPage from "./TrendingMoviesPage";
 
 function App() {
   return (
     <div className="@container max-w-screen-xl mx-auto">
       <Routes>
-        <Route path="/" element={<RatedMovies /> } />
+        <Route path="/" element={<RatedMoviesPage /> } />
+        <Route path="/trending" element={<TrendingMoviesPage /> } />
       </Routes>
     </div>
   )
