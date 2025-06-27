a4de4a7cf3899067701fa3098271e9fceb1cf5b7
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-27 02:28:12 +0200

    Added Navigation component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router";
 import RatedMoviesPage from "./pages/RatedMoviesPage";
 import TrendingMoviesPage from "./pages/TrendingMoviesPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="@container max-w-screen-xl mx-auto">
-      <Routes>
-        <Route path="/" element={<RatedMoviesPage /> } />
-        <Route path="/trending" element={<TrendingMoviesPage /> } />
-      </Routes>
-    </div>
+    <>
+      <Navigation />
+      
+      <div className="@container max-w-screen-xl mx-auto">
+        <Routes>
+          <Route path="/rated" element={<RatedMoviesPage /> } />
+          <Route path="/trending" element={<TrendingMoviesPage /> } />
+        </Routes>
+      </div>
+    </>
   )
 }
 
