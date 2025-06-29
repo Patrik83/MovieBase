52559348be2537faa19ac062f578edc063bfb95c
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-29 12:12:00 +0200

    Added SearchPage component as a route in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import RatedMoviesPage from "./pages/RatedMoviesPage";
 import TrendingMoviesPage from "./pages/TrendingMoviesPage";
 import Navigation from "./components/Navigation";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/rated" element={<RatedMoviesPage /> } />
           <Route path="/trending" element={<TrendingMoviesPage /> } />
+          <Route path="/search" element={<SearchPage /> } />
         </Routes>
       </div>
     </>
