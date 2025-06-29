6b0f1f3da34519170f5da0c9ec7f678203ef263c
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-06-29 12:11:24 +0200

    Added SearchPage component with a search form and rendering of search result

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import { searchMovie } from "../services/TMDB_API";
+import { useSearchParams } from "react-router";
+import { useState } from "react";
+
+const SearchPage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  const query = searchParams.get("query") ?? "";
+
+  const { data: search } = useQuery({
+    queryKey: ["query", query],
+    queryFn: () => searchMovie(query),
+  })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setSearchParams({ query: inputValue });
+  }
+  
+  return (
+    <>
+      <div className="flex justify-end px-4">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            onChange={e => setInputValue(e.target.value)}
+            value={inputValue}
+            className="shadow appearance-none border rounded w-sm py-2 px-3 text-gray-700"
+          />
+          <button 
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >Search</button>
+        </form>
+      </div>
+
+      {search && (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 px-4 pt-3 min-w-[400px]">
+          {search.results.map(result => (
+            <div key={result.id}>
+              <img
+                className="w-full"
+                src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                alt=""
+              />
+              <div className="px-1 py-0">
+                <h2 className="font-bold">{result.title}</h2>
+                <div className="flex justify-between">
+                  <p className="text-gray-700 text-sm">{result.release_date}</p>
+                  <p className="text-gray-700 text-sm">Vote({result.vote_count})</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default SearchPage
