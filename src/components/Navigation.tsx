95e142b253dda7d2f590e153a11d49aa24e159fa
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-07-04 11:50:03 +0200

    Navigation menu is now always in a fixed top position

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,50 +9,45 @@ const Navigation = () => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   return (
-    <nav>
-      <div className="@container max-w-screen-xl mx-auto">
-        <div className="px-2 sm:px-0">
-          <div className="w-full flex items-center justify-between">
-            <div className="sm:hidden min-w-8">
+    <nav className="relative">
+      <div className="fixed w-full z-10 bg-white">
+        <div className="max-w-screen-xl mx-auto px-2 sm:px-0 flex items-center justify-between">
+          <div className="sm:hidden min-w-8">
             <img 
               onClick={() => setIsMenuOpen(!isMenuOpen)} 
               src={burger} alt="Menu" 
               className="w-8 h-8" 
             />
-            </div>
-    
-            {/* desktop view */}
-            <div className="hidden sm:flex px-2 gap-7">
-              <NavLink to={"/trending"}>Trending</NavLink>
-              <NavLink to={"/rated"}>Rated</NavLink>
-              <span className="cursor-pointer" onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>Browse Genres</span>
-            </div>
-
-            <Search />
           </div>
 
-          {/* show this submenu in desktop */}
-          <div className="hidden sm:block px-2 pt-1">
-            {isSubMenuOpen && <GenreList />}
-          </div>
-        </div>
-
-        {/* mobile view */}
-        {isMenuOpen && (
-          <div className="flex flex-col gap-1 sm:hidden bg-gray-300 px-4">
+          <div className="hidden sm:flex px-2 gap-7">
             <NavLink to={"/trending"}>Trending</NavLink>
             <NavLink to={"/rated"}>Rated</NavLink>
             <span className="cursor-pointer" onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>Browse Genres</span>
+          </div>
 
-            {/* show this submenu in mobile */}
-            <div className="px-2 pt-1">
+          <Search />
+        </div>
+
+        <div className="hidden sm:block max-w-screen-xl mx-auto px-2 pt-1">
+          {isSubMenuOpen && <GenreList />}
+        </div>
+      </div>
+
+      <div className="pt-16">
+        {isMenuOpen && (
+          <div className="fixed w-full flex flex-col gap-1 sm:hidden bg-gray-300 px-4 pt-3 z-20">
+            <NavLink to={"/trending"}>Trending</NavLink>
+            <NavLink to={"/rated"}>Rated</NavLink>
+            <span className="cursor-pointer" onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>Browse Genres</span>
+            <div className="px-3 pt-1 pb-3">
               {isSubMenuOpen && <GenreList />}
             </div>
           </div>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navigation;
