5daeabd6d983376bbfbb479822d710447ad6927e
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-07-04 13:33:29 +0200

    Added error and loading states to GenreList component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { getGenres } from "../services/TMDB_API";
+import ErrorAlert from "./ErrorAlert";
+import Spinner from "./Spinner";
 
 const GenreList = () => {
-  const { data: genres } = useQuery({
+  const { data: genres, error, isError, isLoading } = useQuery({
     queryKey: ["genres"],
     queryFn: () => getGenres(),
   })
 
   return (
     <>
+      {isError && <ErrorAlert message={error.message} />}
+
+      {isLoading && <Spinner />}
+
       {genres && (
         <div className="flex flex-wrap justify-start gap-4 text-center bg-gray-300">
           {genres.map(genre => (
