15a4a8b807bc8a68599fc61e18c4950447945d6d
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-07-06 16:41:01 +0200

    Added function to close any open meny when a Link is clicked. Also changed the sub menu outer sides to be transparent

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -16,11 +16,15 @@ const GenreList = () => {
       {isLoading && <Spinner />}
 
       {genres && (
-        <div className="flex flex-wrap justify-start gap-4 text-center bg-gray-300">
-          {genres.map(genre => (
+      <div className="w-full bg-transparent">
+        <div
+          className="max-w-screen-xl mx-auto flex flex-wrap justify-start gap-4 text-center pb-2 pt-2 bg-gray-500"
+        >
+          {genres.map((genre) => (
             <div key={genre.id} className="basis-28">{genre.name}</div>
           ))}
         </div>
+      </div>
       )}
     </>
   )
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,39 +8,47 @@ const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+    setIsSubMenuOpen(false);
+  }
+  
   return (
     <nav className="relative">
       <div className="fixed w-full z-10 dark:bg-black dark:text-white">
         <div className="max-w-screen-xl mx-auto px-2 sm:px-0 flex items-center justify-between">
           <div className="sm:hidden min-w-8">
-            <img 
-              onClick={() => setIsMenuOpen(!isMenuOpen)} 
-              src={burger} 
-              alt="Menu icon" 
-              className="w-8 h-8" 
+            <img
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              src={burger}
+              alt="Menu icon"
+              className="w-8 h-8 cursor-pointer"
             />
           </div>
 
-          <div className="hidden sm:flex px-2 gap-7">
-            <NavLink to={"/trending"}>Trending</NavLink>
-            <NavLink to={"/rated"}>Rated</NavLink>
+          <div className="hidden sm:flex px-0 gap-7">
+            <NavLink to={"/trending"} onClick={handleCloseMenu}>Trending</NavLink>
+            <NavLink to={"/rated"} onClick={handleCloseMenu}>Rated</NavLink>
             <span className="cursor-pointer" onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>Browse Genres</span>
           </div>
 
           <Search />
         </div>
 
-        <div className="hidden sm:block max-w-screen-xl mx-auto px-2 pt-1">
-          {isSubMenuOpen && <GenreList />}
-        </div>
+        {isSubMenuOpen && (
+          <div className="hidden sm:block w-full absolute pt-1 z-10">
+            <GenreList />
+          </div>
+        )}
       </div>
 
       <div className="pt-16">
         {isMenuOpen && (
           <div className="fixed w-full flex flex-col gap-1 sm:hidden bg-gray-300 px-4 pt-3 z-20">
-            <NavLink to={"/trending"}>Trending</NavLink>
-            <NavLink to={"/rated"}>Rated</NavLink>
+            <NavLink to={"/trending"} onClick={handleCloseMenu}>Trending</NavLink>
+            <NavLink to={"/rated"} onClick={handleCloseMenu}>Rated</NavLink>
             <span className="cursor-pointer" onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>Browse Genres</span>
+
             <div className="px-3 pt-1 pb-3">
               {isSubMenuOpen && <GenreList />}
             </div>
