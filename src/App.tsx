a056a1df367420a8859cd5d19dceb430a439f90a
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-07-23 00:57:06 +0200

    Added HomePage component as a route to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import RatedMoviesPage from "./pages/RatedMoviesPage";
 import SearchPage from "./pages/SearchPage";
 import TrendingMoviesPage from "./pages/TrendingMoviesPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       
       <div className="@container max-w-screen-xl mx-auto">
         <Routes>
-          <Route path="/rated" element={<RatedMoviesPage /> } />
-          <Route path="/trending" element={<TrendingMoviesPage /> } />
-          <Route path="/search" element={<SearchPage /> } />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/rated" element={<RatedMoviesPage />} />
+          <Route path="/trending" element={<TrendingMoviesPage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Routes>
       </div>
     </div>
