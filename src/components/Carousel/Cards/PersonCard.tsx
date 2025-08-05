1e1aa595890a2f06be91810469eec253f4449ea4
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-08-05 22:41:04 +0200

    Adjusted indentation in PersonCard

--- src/components/Carousel/Cards/PersonCard.tsx
+++ src/components/Carousel/Cards/PersonCard.tsx
@@ -6,11 +6,11 @@ interface PersonCardProps {
 
 const PersonCard: React.FC<PersonCardProps> = ({ item }) => {
   return (
-      <img
-        src={`https://image.tmdb.org/t/p/w342/${item.file_path}`}
-        alt=""
-        className="mx-auto sm:mx-0 px-3 w-72 h-auto lg:w-100"
-      />
+    <img
+      src={`https://image.tmdb.org/t/p/w342/${item.file_path}`}
+      alt=""
+      className="mx-auto sm:mx-0 px-3 w-72 h-auto lg:w-100"
+    />
   )
 }
 
