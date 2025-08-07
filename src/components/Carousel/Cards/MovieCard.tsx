1a536f252dda7b1ec42cc4a6545af5d580c041f1
Patrik Norrgård <patrik.norrgard@yahoo.se>
2025-08-07 16:31:18 +0200

    Added the use of 'size' prop to MovieCard

--- src/components/Carousel/Cards/MovieCard.tsx
+++ src/components/Carousel/Cards/MovieCard.tsx
@@ -3,17 +3,20 @@ import type { Movie } from "../../../types/Movie.types";
 
 interface MovieCardProps {
   item: Movie;
+  size?: string;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ item }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ item, size }) => {
   return (
     <Link to={`/movie/${item.id}`}>
       <img
         src={`https://image.tmdb.org/t/p/w342/${item.poster_path}`}
         alt=""
+        className={`block px-2 ${size}`}
       />
     </Link>
-  )
-}
+  );
+};
+
 
 export default MovieCard;
